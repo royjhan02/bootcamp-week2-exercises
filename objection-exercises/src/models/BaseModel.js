3a133ae107817c5b4c33bcafa5260e66e9346608
Charles Onesti <charlesgonesti@gmail.com>
2021-01-12 12:48:56 -0500

    Update BaseModel.js

--- objection-exercises/src/models/BaseModel.js
+++ objection-exercises/src/models/BaseModel.js
@@ -4,10 +4,6 @@ const knex = require('../lib/index')
 Model.knex(knex)
 
 class BaseModel extends Model {
-  static get columnNameMappers() {
-    return snakeCaseMappers()
-  }
-
 }
 
 module.exports = BaseModel
